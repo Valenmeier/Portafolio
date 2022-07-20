0ec730dbe490a460c5e17d8d39a7ede26479f81b
Valentin Meier <valentinmeier123@gmail.com>
2022-07-20 03:18:22 -0300

    primer commit

---
+++ javascript/scripts.js
@@ -0,0 +1,100 @@
+const inyectar= document.querySelector(`.inyeccion`) //lugar a inyectar codigo
+let seccionActual;
+//Mostrar inicio 
+const mostrarInicio=()=> {
+    // inyectar.innerHTML=""
+    // inyectar.innerHTML= inicio
+}
+mostrarInicio()
+//Secciones
+const sobreMi=() => {
+    // inyectar.innerHTML=""
+    // inyectar.innerHTML= miInformacion
+}
+const proyectos=() => {
+    // inyectar.innerHTML=""
+    // inyectar.innerHTML= misProyectos
+}
+//Cambiar tema al ingresar
+let cambiarTemaIngreso = () => {
+    let cambiarTema=JSON.parse(localStorage.getItem(`tema`)) || `light`
+    let body= document.querySelector(`.eleccion`)
+    body.classList.add(cambiarTema)
+}
+cambiarTemaIngreso()
+//activar eventos de temas
+let activarModalTemas= ()=> {
+    let temas=document.querySelector(`.modo`)
+    temas.addEventListener(`click`,()=> {
+        let abrirModal=document.querySelector(`.temas`)
+        abrirModal.classList.toggle(`abrirMenuTemas`)
+        
+    })
+}
+activarModalTemas()
+//Cerrar modal
+let cerrarModal=()=> {
+    let abrirModal=document.querySelector(`.temas`)
+        abrirModal.classList.toggle(`abrirMenuTemas`)
+}
+//desactivar evento de temas
+let desactivarModalTemas=()=> {
+    let cerrar=document.querySelector(`.cerrar`)
+    cerrar.addEventListener(`click`,()=> {
+        cerrarModal()
+    })
+}
+desactivarModalTemas()
+//cambiar color de temas
+let cambiarTemas= ()=> {
+    let temaClaro=document.querySelector(`.Claro`)
+    let temaOscuro=document.querySelector(`.Oscuro`)
+    let temaMounstruo=document.querySelector(`.Monstruo`)
+    let temaDivino=document.querySelector(`.Divino`)
+    let temaDracula=document.querySelector(`.Dracula`)
+    let cambiadorDeTemas= (tema,nombreDeReemplazo)=> {
+        tema.addEventListener(`click`,()=> {
+            let body=document.querySelector(`body`)
+            body.classList.replace(body.classList[1],`${nombreDeReemplazo}`)
+            recordarTema()
+            cerrarModal()
+        })
+    }
+    cambiadorDeTemas(temaClaro,`light`)
+    cambiadorDeTemas(temaOscuro,`dark`)
+    cambiadorDeTemas(temaMounstruo,`monster`)
+    cambiadorDeTemas(temaDivino,`divinity`)
+    cambiadorDeTemas(temaDracula,`dracula`)
+}
+cambiarTemas()
+//recordar tema para reingreso
+let recordarTema=()=> {
+    let body=document.querySelector(`body`)
+    let temaAGuardar= body.classList[1]
+    localStorage.setItem(`tema`,JSON.stringify(temaAGuardar))
+}
+
+
+//Cambiar animación de botones
+let botones= document.querySelectorAll(`.inicio-footer`)
+
+for (i=0;i<botones.length;i++) {
+    let boton=botones[i]
+   boton.addEventListener(`click`,()=> {
+    botonesActivos=document.querySelector(`.retoques`)
+    botonesActivos.classList.remove(`retoques`)
+    boton.classList.toggle(`retoques`)
+   })
+}
+
+//Cambiar seccion
+
+botones[0].addEventListener(`click`,()=> {
+    mostrarInicio()
+})
+botones[1].addEventListener(`click`,()=> {
+    sobreMi()
+})
+botones[2].addEventListener(`click`,()=> {
+    proyectos()
+})
---
+++ javascript/secciones.js
@@ -0,0 +1,37 @@
+//Secciones del documento
+let inicio = `
+<div class="main-inicio">
+        <h1>Hola mundo!<br>
+        Yo soy <span>Valentin Meier</span><br>
+    <span>{</span>Desarrollador Front-end<span>}</span></h1> 
+        <h2><span>Sean bienvenidos a mi portafolio...</span></h2>
+        <p class="introduccion">Antes de comenzar, les voy contar un poco sobre mí... vivo en Villa Carlos Paz-Argentina.<br> Soy una persona <span><b>social</b></span> a la que le encanta <span><b>aprender</b></span> cosas nuevas, disfruta <span><b>crear</b></span> y <span><b>enseñar</b></span> lo aprendido.<br> Ahora si, espero disfruten de mi sitio.</p>
+        <div class="imagen-inicio">
+            <img src="./multimedia/Portafolio.jpeg" alt="Valentin Meier">
+        </div>
+</div>
+`
+let miInformacion= `<div class="main-sobreMi">
+<h4>Sobre <span>Mi</span></h4>
+
+    <div class="habilidades">
+    <h5>Bio</h5>
+    <a href="#habilidades"><h6>habilidades</h6></a>
+</div>
+<p>Nací el 14/01/2003 en Córdoba-Argentina. Desde muy chiquito desarrolle el amor por la tecnología... en ese tiempo me gustaba editar videos y corromper mis jueguitos para sacar beneficios. Con el paso del tiempo fuí creciendo y encontrando otras cosas que llamaban mi atención (la economia y los deportes)... pero nada me hacia sentir tan bien como cuando utilizaba la maquina y aprendia cosas nuevas, cosas únicas.<br> En estos ultimos años, luego de haber finalizado la secundaria, entre en un lapso del no saber que hacer, empece estudiando economia, pero luego me arrepentí, no me sentia tan comodo. Por tanto quise buscar trabajo, pensando que así podria ser más feliz, lo conseguí (ingrese a Arcos Dorados) y la verdad que tenia razón, al principio costo muchisimo, tenia muchos compañeros (200 aproximadamente) y me sentia bastante perdido. Pero eso me motivo a aprender, cometí muchos errores (algunos más de una vez), tenia compañeros que se burlaban por esos fallos, sinceramente me sentia mal cuando me trataban asi, pero siempre tomaba su burla con una sonrisa y seguia, nunca le falte el respeto a ningún compañero. Gracias a mi esfuerzo y buen trato logre conseguir uno de mis mayores logros, el diploma de empleado del mes. </p>
+<img src="./multimedia/diploma.jpeg" alt="diploma empleado del mes" title="mi diploma" class="diploma">
+<p>Luego de eso decidi renunciar para poder seguir aprendiendo más cosas (siempre recordando todos los momentos y experiencias que pase en aquel lugar), me anote a coderhouse, donde actualmente estoy cursando y conseguí completar 2 cursos (desarrollo web y javascript) en esos cursos desarrolle las siguientes herramientas:</p><br>
+
+<div class="herramientas" id="habilidades">
+    <i class="fa-brands fa-html5"></i>
+    <i class="fa-brands fa-css3-alt"></i>
+    <i class="fa-brands fa-js"></i>
+    <i class="fa-brands fa-git-alt"></i>
+    <i class="fa-brands fa-github"></i>
+    <i class="fa-brands fa-bootstrap"></i>
+    <i class="fa-brands fa-sass"></i>
+</div><br>
+
+<p class="parrafoFinal">Como anteriormente mencione, aún sigo cursando en Coderhouse (me faltan dos cursos para completar la carrera de full-stack), por tanto mis habilidades van a seguir creciendo, tanto en el front-end, como en el back-end. <br>Sin nada más que decir, muchas gracias por haberme leido, espero sigas disfrutando mi portafolio.</p>
+</div>`
+let misProyectos=``
